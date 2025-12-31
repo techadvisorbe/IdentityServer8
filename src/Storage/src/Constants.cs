b1a373b98235b2fe264a089e484d0b2236328e7f
Abdelilah El mansouri <contact@techadvisor.be>
2025-12-31 13:54:55 +0100

    Release

--- src/Storage/src/Constants.cs
+++ src/Storage/src/Constants.cs
@@ -15,5 +15,6 @@ namespace IdentityServer8;
 internal static class Constants
 {
     public const string IdentityServerName               = "IdentityServer8";
+    
     public const string IdentityServerAuthenticationType = IdentityServerName;
 }
